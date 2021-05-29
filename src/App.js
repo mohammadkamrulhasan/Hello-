c27ed2189561f49dc31b9e678d6e218d4504febf
mohammadkamrulhasan <kamrul786in@gmail.com>
2021-05-30 01:46:26 +0600

    Create New branch and update Home page and footer part

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
 import logo from './logo.svg';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import './App.css';
+import Home from './components/Home/Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+    <Home></Home>
+    <div>
+      <Switch>
+        <Route path="/">
+         
+        </Route>
+        <Route path="/">
+        
+        </Route>
+        <Route path="/">
+         
+        </Route>
+        <Route path="/">
+        
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
     </div>
+  </Router>
   );
 }
 
---
+++ src/components/Home/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Footer from '../Footer/Footer';
+import Review from '../Review/Review';
+import Service from '../Services/Services';
+
+const Home = () => {
+    return (
+        <div>
+            <Service></Service>
+            <Review></Review>
+            <Footer></Footer>
+        </div>
+    );
+};
+
+export default Home;
