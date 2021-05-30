ec30b056cd43afe3189ad600c2fecf3a74d85bd7
mohammadkamrulhasan <kamrul786in@gmail.com>
2021-05-30 18:13:58 +0600

    Responsive Home page Update

--- src/components/Header/HeaderMain.js
+++ src/components/Header/HeaderMain.js
@@ -4,7 +4,7 @@ import './HeaderMain.css';
 
 const HeaderMain = () => {
     return (
-        <main style={{height:'600px',marginTop:'60px'}} className="row d-flex align-items-center">
+        <main style={{height:'600px',marginTop:'60px', marginBottom:'60px', overflow:'hidden'}} className="row d-flex align-items-center">
             <div className="col-md-4 offset-md-1">
                 <h1 style={{color: '#3A4256'}}>Let's Chat or  <br/>Make Video Call  </h1>
                 <p >We are providing Chat,Video Call and Video Conference service.If your are interested to chat or video call with your special one. Click Below.</p>
