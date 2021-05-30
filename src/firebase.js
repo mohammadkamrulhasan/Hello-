2879615f14d0020f2d4d65611224779c48a52e10
omar meharab <omarmeharab81@gmail.com>
2021-05-30 16:28:38 +0600

    chat sidebar nearly done next will do redux

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from "firebase";
+
 const firebaseConfig = {
     apiKey: "AIzaSyDIa261GY3Z9n2-3PBwql8IChQVCBb0K48",
     authDomain: "hello-cc594.firebaseapp.com",
@@ -6,3 +8,11 @@ const firebaseConfig = {
     messagingSenderId: "962754996046",
     appId: "1:962754996046:web:e7a9e78b1b777beba3201f"
   };
+
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+  const db = firebaseApp.firestore();
+  const auth = firebase.auth()
+  const provider = new firebase.auth.GoogleAuthProvider();
+
+  export { auth, provider, db}
