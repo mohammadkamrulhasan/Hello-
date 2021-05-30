3f306feebe217b5ee0c283910a08b3ba04f35688
ahmedali0159 <pirates0159@gmail.com>
2021-05-30 16:48:17 +0600

    added about

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -2,9 +2,19 @@ import React from 'react';
 
 const About = () => {
     return (
-        <div>
-            <h1>About</h1>
+        <div className="container">
+        <div style={{paddingLeft: "25px"}}>
+            <h1>About Us</h1>
+            <p className="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis exercitationem fugiat tenetur temporibus cumque dignissimos perferendis magni facere animi at aut voluptas esse obcaecati error, possimus hic eveniet velit pariatur?</p>
+
+
+            <p className="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis exercitationem fugiat tenetur temporibus cumque dignissimos perferendis magni facere animi at aut voluptas esse obcaecati error, possimus hic eveniet velit pariatur?</p>
+
+            
+            <p className="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis exercitationem fugiat tenetur temporibus cumque dignissimos perferendis magni facere animi at aut voluptas esse obcaecati error, possimus hic eveniet velit pariatur?</p>
         </div>
+        
+    </div>
     );
 };
 
